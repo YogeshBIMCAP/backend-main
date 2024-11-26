cb8c162a64696288c8ad17a797490e9765581355
YogeshSen <YogeshSen@BIMCAP_INDIA_YS>
2024-11-26 15:38:28 +0530

    get to post

--- routes/department.routes.js
+++ routes/department.routes.js
@@ -3,7 +3,7 @@ import { getAllDepartments } from "../controllers/department.controller.js";
 const router = express.Router();
 
 
-router.get("/all",getAllDepartments);
+router.post("/all",getAllDepartments);
 
 // Export the router
 export default router;
