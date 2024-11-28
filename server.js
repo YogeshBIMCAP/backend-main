3f7e42457e7c4d0761c452e68ecddc6be28ba628
YogeshSen <YogeshSen@BIMCAP_INDIA_YS>
2024-11-28 11:21:21 +0530

    .

--- server.js
+++ server.js
@@ -7,40 +7,34 @@ import path from "path";
 const app = express();
 
 configDotenv({
-    path: './.env',
+  path: "./.env",
 });
 
-app.use(cors({
-    origin: "*"
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Directly add the new /code route
-app.get("/", (req, res) => {
-    const code = req.query.code; // Extract the 'code' parameter
-    console.log("Extracting code...");
+app.get("/", async(req, res) => {
+  const code = req.query.code; // Extract the 'code' parameter
+  console.log("Extracting code...");
 
-    if (code) {
-        console.log(`Extracted code: ${code}`);
-        
-        const filePath = path.join(process.cwd(), "extracted_code.txt");
-        
-        // Check if the file exists
-        if (!fs.existsSync(filePath)) {
-            // Create the file and write the code
-            fs.writeFileSync(filePath, code, "utf8");
-            console.log("New file created and code written to it.");
-        } else {
-            console.log("File already exists. Appending code to it.");
-            fs.appendFileSync(filePath, `\n${code}`, "utf8"); // Append the code
-        }
+  if (code) {
+    console.log(`Extracted code: ${code}`);
 
-        res.send(`Extracted code: ${code} and written to file.`);
-    } else {
-        res.send("No code parameter found in the query string.");
-    }
+    const result = await axios.get(
+      `https://bimcap.bitrix24.com/oauth/token/?grant_type=authorization_code&client_id=local.674800c04736c3.51073976&client_secret=2SkIUBpq4Dml6UB5MHZhX1OJ12jhnSvwLk1q23ZLgyOjbvKXAc&code=${code}&scope=application_permissions&redirect_uri=application_URL`
+    );
+
+    res.send(`result: ${result}`);
+  } else {
+    res.send("No code parameter found in the query string.");
+  }
 });
 
 // Import existing routes
@@ -57,9 +51,7 @@ app.use("/task", taskRoutes);
 app.use("/report", reportRoutes);
 app.use("/department", departmentRoutes);
 
-
-
 // Start the server
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
